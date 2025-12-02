5b6cfd6fccd790c8e354edfb26f4522c85e08dd7
thilak <tsbalagangadharthilak1431@gmail.com>
2025-12-02 19:44:44 +0530

    Added few lines in Offshore team

--- src/gitpractice/GitPractice.java
+++ src/gitpractice/GitPractice.java
@@ -4,5 +4,7 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("First Time Added in Git Demo");
         System.out.println("Second Time Added in Git Demo");
+        System.out.println("From Offshore Team");
+        System.out.println("From Offshore Team");
     }
 }
