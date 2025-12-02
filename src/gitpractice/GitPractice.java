58c7a60d640f8e1f79595afb55dd8dbc4fba9175
thilak <tsbalagangadharthilak1431@gmail.com>
2025-12-02 19:28:29 +0530

    Added Git package and 2 sout onsite time

---
+++ src/gitpractice/GitPractice.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class GitPractice {
+    public static void main(String[] args) {
+        System.out.println("First Time Added in Git Demo");
+        System.out.println("Second Time Added in Git Demo");
+    }
+}
